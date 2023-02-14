c036fb4b2f586096929e65f79310f4ee7b5d97b2
hdlee0619 <dlgusehd1211@naver.com>
2023-02-15 02:11:01 +0900

    design: Modal 컴포넌트 추가 및 디자인 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ButtonContainer from './components/ButtonContainer';
 import InputContainer from './components/InputContainer';
+import ModalContainer from './components/ModalContainer';
 import GlobalStyle from './style/GlobalStyle';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <GlobalStyle />
       <ButtonContainer></ButtonContainer>
       <InputContainer></InputContainer>
+      <ModalContainer></ModalContainer>
     </>
   );
 }
