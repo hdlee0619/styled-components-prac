5ef340307a4e79f9a76833fece2ece08f0aa1cae
hdlee0619 <dlgusehd1211@naver.com>
2023-02-15 15:56:04 +0900

    feat: 모달창 팝업 시 스크롤 막기 기능 추가

--- src/components/Modal/ModalBox.jsx
+++ src/components/Modal/ModalBox.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Btn from '../Btn';
 import styled from 'styled-components';
 
 function ModalBox({ children, onClose }) {
+  useEffect(() => {
+    const $body = document.querySelector('body');
+    console.log('$body -> ', $body);
+    const overflow = $body.style.overflow;
+    $body.style.overflow = 'hidden'; // body를 hidden 으로 변경
+    // modal 컴포넌트가 사라졌을 때 body를 다시 스크롤 가능하게 만들어주도록 클린업 사용
+    return () => {
+      $body.style.overflow = overflow;
+    };
+  }, []);
+
   return (
     <ModalBg>
       <ModalWrapper>
